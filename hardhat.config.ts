53d433c3c4f0758678b3561233100257fc670050
9oodam <godamcity98@gmail.com>
2025-11-05 18:05:11 +0900

    token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const privateKey = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : "";
+const privateKey2 = process.env.PRIVATE_KEY_2 ? process.env.PRIVATE_KEY_2 : "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_ALCHEMY_RPC,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      accounts: [privateKey, privateKey2],
     },
   },
 };
